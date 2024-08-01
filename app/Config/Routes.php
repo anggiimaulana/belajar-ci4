78f3182af1c68a1c7ae1fe08dd896099db7f8ee8
anggii <anggabos120@gmail.com>
2024-08-01 07:56:42 +0700

    commit views

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,22 +5,22 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Pages::index');
 
-// nilai khusus
-$routes->get('/coba/index', 'Coba::index');
-$routes->get('/coba/about', 'Coba::about');
+// // nilai khusus
+// $routes->get('/coba/index', 'Coba::index');
+// $routes->get('/coba/about', 'Coba::about');
 
-// mengambil nilai dari url 1 placeholder
-$routes->get('/coba/(:any)', 'Coba::about/$1');
+// // mengambil nilai dari url 1 placeholder
+// $routes->get('/coba/(:any)', 'Coba::about/$1');
 
-// mengambil nilai dari url 2 placeholder
-$routes->get('/coba/(:any)/(:num)', 'Coba::about/$1/$2');
+// // mengambil nilai dari url 2 placeholder
+// $routes->get('/coba/(:any)/(:num)', 'Coba::about/$1/$2');
 
-$routes->get('/users', 'Admin\Users::index');
-// perilaku route
-// $routes->get('/coba', function () {
-//     echo "Hello World";
-// });
+// $routes->get('/users', 'Admin\Users::index');
+// // perilaku route
+// // $routes->get('/coba', function () {
+// //     echo "Hello World";
+// // });
 
 $routes->setAutoRoute(true);
---
+++ app/Views/Pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem</p>
+        </div>
+    </div>
+</div>
