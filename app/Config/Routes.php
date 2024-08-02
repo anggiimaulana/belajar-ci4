47d6c1096a7a64d2e76fd7ee03b4f80d7fb027da
anggii <anggabos120@gmail.com>
2024-08-02 10:30:53 +0700

    commit model part 2

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Pages::index');
+$routes->get('/komik/(:segment)', 'Komik::detail/$1');
 
 // // nilai khusus
 // $routes->get('/coba/index', 'Coba::index');
--- app/Controllers/Komik.php
+++ app/Controllers/Komik.php
@@ -16,24 +16,23 @@ class Komik extends BaseController
 
     public function index() 
     {
-        $komik = $this->komikModel->findAll();
+        // $komik = $this->komikModel->findAll();
 
         $data = [
             'title' => 'Daftar Komik',
-            'komik' => $komik
+            'komik' => $this->komikModel->getKomik()
         ];
 
-        // $komikModel = new \App\Models\KomikModel();
-        // $komikModel = new KomikModel();
-        
-
-        // cara konek db tanpa model
-        // $db = \Config\Database::connect();
-        // $komik = $db->query("SELECT * FROM komik");
-        // foreach($komik->getResultArray() as $row);
-        // d($row);
-
         return view('komik/index', $data);
+    }
+
+    public function detail($slug)
+    {
+        $data = [
+            'title' => 'Detail Komik',
+            'komik' => $this->komikModel->getKomik($slug)
+        ];
+        return view('komik/detail', $data);
     }
 }
 
